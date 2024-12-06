5e6c046d953c4cbf84d2e524d5d64fb3476453ad
MohamedV3 <lakhdaralix@gmail.com>
2024-12-06 15:22:21 +0100

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import NavBar from "./NavBar";
+
 function App() {
   return (
-    <div className="conainer mx-auto p-5 bg-[url('public/background.jpg')] h-screen">
+    <div className="conainer mx-auto p-5 bg-[url('/public/background.jpg')] h-screen">
       <NavBar />
     </div>
   );
